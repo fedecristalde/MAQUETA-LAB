afba9b3dd80edc697d7b6c28adcd564270d8a33a
fedecristalde <fecristalde@mi.unc.edu.ar>
2026-04-09 19:51:59 -0300

    Update ALU.sv

--- ALU.sv
+++ ALU.sv
@@ -43,35 +43,35 @@ module alu(
     logic sum0, sum1, sum2, sum3; 
 
     adder_1bit bit0 (
-    .a(a_in_0), 
-    .b(b_in_0), 
-    .carry_in(1'b0), //acarreo inicial es 0
-    .add(sum0), 
-    .carry_out(c0)
+        .a(a_in_0), 
+        .b(b_in_0), 
+        .carry_in(1'b0), //acarreo inicial es 0
+        .add(sum0), 
+        .carry_out(c0)
     );
 
     adder_1bit bit1 (
-    .a(a_in_1), 
-    .b(b_in_1), 
-    .carry_in(c0),
-    .add(sum1), 
-    .carry_out(c1)
+        .a(a_in_1), 
+        .b(b_in_1), 
+        .carry_in(c0),
+        .add(sum1), 
+        .carry_out(c1)
     );
 
     adder_1bit bit2 (
-    .a(a_in_2), 
-    .b(b_in_2), 
-    .carry_in(c1),
-    .add(sum2), 
-    .carry_out(c2)
+        .a(a_in_2), 
+        .b(b_in_2), 
+        .carry_in(c1),
+        .add(sum2), 
+        .carry_out(c2)
     );
 
     adder_1bit bit3 (
-    .a(a_in_3), 
-    .b(b_in_3), 
-    .carry_in(c2),
-    .add(sum3), 
-    .carry_out(carry_flag_add)
+        .a(a_in_3), 
+        .b(b_in_3), 
+        .carry_in(c2),
+        .add(sum3), 
+        .carry_out(carry_flag_add)
     );
 
     
@@ -87,41 +87,95 @@ module alu(
     not_gate u_not3 (.a(b_in_3), .y(nb3));
     
     adder_1bit bit00 (
-    .a(a_in_0), 
-    .b(nb0), 
-    .carry_in(1'b1), //acarreo inicial es 1 (el bit que debo sumar para el c2)
-    .add(sum00), 
-    .carry_out(c00)
+        .a(a_in_0), 
+        .b(nb0), 
+        .carry_in(1'b1), //acarreo inicial es 1 (el bit que debo sumar para el c2)
+        .add(sum00), 
+        .carry_out(c00)
     );
 
     adder_1bit bit01 (
-    .a(a_in_1), 
-    .b(nb1), 
-    .carry_in(c00),
-    .add(sum01), 
-    .carry_out(c01)
+        .a(a_in_1), 
+        .b(nb1), 
+        .carry_in(c00),
+        .add(sum01), 
+        .carry_out(c01)
     );
 
     adder_1bit bit02 (
-    .a(a_in_2), 
-    .b(nb2), 
-    .carry_in(c01),
-    .add(sum02), 
-    .carry_out(c02)
+        .a(a_in_2), 
+        .b(nb2), 
+        .carry_in(c01),
+        .add(sum02), 
+        .carry_out(c02)
     );
 
     adder_1bit bit03 (
-    .a(a_in_3), 
-    .b(nb3), 
-    .carry_in(c02),
-    .add(sum03), 
-    .carry_out(carry_flag_sub)
+        .a(a_in_3), 
+        .b(nb3), 
+        .carry_in(c02),
+        .add(sum03), 
+        .carry_out(carry_flag_sub)
     );
 
     //------------------------- PIB (111) -------------------------
     
     //---------------------------- MUX ----------------------------
+    mux8_1 alu_mux_0 (
+        .d0(ca0),    // Resultado AND bit 0
+        .d1(co0),     // Resultado OR bit 0
+        .d2(sum0),        // Resultado Suma bit 0
+        .d3(1'b0),        // Reservado
+        .d4(1'b0),        // Reservado
+        .d5(1'b0),        // Reservado
+        .d6(sum00),       // Resultado Resta bit 0
+        .d7(b_in_0),      // Pass Input B bit 0 // AGREGAR ACLARACION DE PIB
+        .sel_0(alu_op_0), 
+        .sel_1(alu_op_1), 
+        .sel_2(alu_op_2),
+        .y(alu_result_0)  // Salida final del bit 0);
 
+    mux8_1 alu_mux_1 (
+        .d0(ca1),    // Resultado AND bit 0
+        .d1(co1),     // Resultado OR bit 0
+        .d2(sum1),        // Resultado Suma bit 0
+        .d3(1'b0),        // Reservado
+        .d4(1'b0),        // Reservado
+        .d5(1'b0),        // Reservado
+        .d6(sum01),       // Resultado Resta bit 0
+        .d7(b_in_1),      // Pass Input B bit 0 // AGREGAR ACLARACION DE PIB
+        .sel_0(alu_op_0), 
+        .sel_1(alu_op_1), 
+        .sel_2(alu_op_2),
+        .y(alu_result_1)  // Salida final del bit 0);
+
+     mux8_1 alu_mux_2 (
+        .d0(ca2),    // Resultado AND bit 0
+        .d1(co2),     // Resultado OR bit 0
+        .d2(sum2),        // Resultado Suma bit 0
+        .d3(1'b0),        // Reservado
+        .d4(1'b0),        // Reservado
+        .d5(1'b0),        // Reservado
+        .d6(sum02),       // Resultado Resta bit 0
+        .d7(b_in_2),      // Pass Input B bit 0 // AGREGAR ACLARACION DE PIB
+        .sel_0(alu_op_0), 
+        .sel_1(alu_op_1), 
+        .sel_2(alu_op_2),
+        .y(alu_result_2)  // Salida final del bit 0);
+    
+         mux8_1 alu_mux_3 (
+        .d0(ca3),    // Resultado AND bit 0
+        .d1(co3),     // Resultado OR bit 0
+        .d2(sum3),        // Resultado Suma bit 0
+        .d3(1'b0),        // Reservado
+        .d4(1'b0),        // Reservado
+        .d5(1'b0),        // Reservado
+        .d6(sum03),       // Resultado Resta bit 0
+        .d7(b_in_3),      // Pass Input B bit 0 // AGREGAR ACLARACION DE PIB
+        .sel_0(alu_op_0), 
+        .sel_1(alu_op_1), 
+        .sel_2(alu_op_2),
+        .y(alu_result_3)  // Salida final del bit 0);
     //--------------------------- FLAGS ---------------------------
     
     
