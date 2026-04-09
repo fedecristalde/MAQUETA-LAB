0d7b61137cd7c9179ac45db102d6c52fa1b9772f
fedecristalde <fecristalde@mi.unc.edu.ar>
2026-04-09 16:13:25 -0300

    Update ALU.sv

--- ALU.sv
+++ ALU.sv
@@ -27,29 +27,68 @@ module alu(
     //------------------------- OR  (001) -------------------------
     
     //------------------------- ADD (010) -------------------------
-`timescale 1ns / 1ps
 
-module adder_1bit_0(
-    input a_in_0,
-    input b_in_0,
-    input carry_in_0,
-    output s1,
-    output carry_out_0
+    `timescale 1ns / 1ps
+
+module adder_1bit(
+    input a,
+    input b,
+    input carry_in,
+    output add,
+    output carry_out
     );
 
-    wire s0;
-    wire c0,c1;
+    wire s1;
+    wire c1,c2;
 
-    xor(s0, a_in_0,b_in_0);
-    and(c0,a_in_0,b_in_0);
-
-    assign carry_in_0 = 0;
+    xor(s1, a,b);
+    and(c1,a,b);
     
-    xor(s1,s0,carry_in_0);
-    and(c1,s0,carry_in_0);
+    xor(add,s1,carry_in);
+    and(c2,s1,carry_in);
 
-    or(carry_out_0,c0,c1);
-    
+    or(carry_out,c1,c2);
+
+module adder_4bit(
+    input  a0, a1, a2, a3,
+    input  b0, b1, b2, b3,
+    output s0, s1, s2, s3,
+    output carry_out
+);
+    wire c0, c1, c2;
+   
+    adder_1bit bit0(
+        .a(a0),
+        .b(b0),
+        .carry_in(1'b0),  // (1'b0) representa el cero de un bit.
+        .add(s0),
+        .carry_out(c0)
+         );
+    adder_1bit bit1(
+        .a(a1),
+        .b(b1),
+        .carry_in(c0),    
+        .add(s1),
+        .carry_out(c1)
+        );
+
+    adder_1bit bit2(
+        .a(a2),
+        .b(b2),
+        .carry_in(c1),    
+        .add(s2),
+        .carry_out(c2)
+        );
+
+    adder_1bit bit3(
+        .a(a3),
+        .b(b3),
+        .carry_in(c2),    
+        .add(s3),
+        .carry_out(carry_out)  
+        );
+
+endmodule
    
     
          
